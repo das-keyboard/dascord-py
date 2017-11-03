fce12b4a43ba1df95e5749e98a661957bd8dd66e
das_keyboard <DasKeyboard@protonmail.com>
2017-11-03 21:41:09 +0100

    "Error-Handling" for wiki-command.

--- lib_wikipedia.py
+++ lib_wikipedia.py
@@ -3,10 +3,13 @@ import wikipedia
 
 def wikisum(search: str, local: str = 'de', firstsentence: int = 0):
     wikipedia.set_lang(local)
-    if firstsentence == 1:
-        data = wikipedia.summary(search, sentences=1)
-    else:
-        data = wikipedia.summary(search)
-    if not data:
+    try:
+        if firstsentence == 1:
+            data = wikipedia.summary(search, sentences=1)
+        else:
+            data = wikipedia.summary(search)
+        if not data:
+            return "Nothing found :("
+    except:
         return "Nothing found :("
     return data
